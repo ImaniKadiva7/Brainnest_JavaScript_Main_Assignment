ead6896c0e5634d8c0fb97f297915b16c05175e1
ImaniKadiva7 <kadivaimani@yahoo.com>
2023-03-16 13:03:26 +0000

    seventh commit

--- script.js
+++ script.js
@@ -1,172 +1,236 @@
 
-const gameOptions = ["rock", "paper", "scissors"],
-      off = 2,
-      on = 0;
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const finalColumn = document.querySelector('[data-final-column]');
+const computerScoreSpan = document.querySelector('[data-computer-score]');
+const yourScoreSpan = document.querySelector('[data-your-score]');
+const gameOptions = [
+    {
+        name: 'rock',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        beats: 'paper'
+    }
+]
 
+selectionButtons.forEach(selectionButtons => {
+    selectionButtons.addEventListener('click', e => {
+        const selectionName = selectionButtons.dataset.selection;
+        const selection = gameOptions.find(selection => selection.name === selectionName)
+        makeSelection(selection);
+    })
+})
 
-function computerPlay()
+function makeSelection(selection)
 {
-    let computerSelection = gameOptions[Math.floor((Math.random() * gameOptions.length) + 0)];
-    return computerSelection;
-}
+    const computerSelection = randomSelection();
+    const yourWinner = isWinner(selection, computerSelection);
+    const computerWinner = isWinner(computerSelection, selection);
 
-
-
-function playRound(playerSelection, computerSelection)
-{
-    let Messages = messageResponses(playerSelection, computerSelection);
-    if( playerSelection == gameOptions[1] && computerSelection == gameOptions[2] )
-    {
-        return Messages.playerLoseMessage;
-    }
-    else if( playerSelection ==gameOptions[0] && computerSelection == gameOptions[2] )
-    {
-        return Messages.playerWinsMessage;
-    }
-    else if(playerSelection ==gameOptions[0] && computerSelection == gameOptions[1] )
-    {
-        return Messages.playerLoseMessage;
-    }
-    else if(playerSelection == gameOptions[2] && computerSelection == gameOptions[1] )
-    {
-        return Messages.playerLoseMessage;
-    }
-    else if(playerSelection == gameOptions[1] && computerSelection ==gameOptions[0] )
-    {
-        return Messages.playerWinsMessage;
-    } 
-    else if(playerSelection == gameOptions[2] && computerSelection ==gameOptions[0] )
-    {
-        return Messages.playerLoseMessage;
-    }
-    else
-    {
-        return Messages.gameTieMessage;
-    }
+    addSelectionResult(computerSelection, computerWinner);
+    addSelectionResult(selection, yourWinner);
     
+    if(yourWinner) {incrementScore(yourScoreSpan);}
+    else if(computerWinner) {incrementScore(computerScoreSpan)};
 }
-function messageResponses(playerSelection, computerSelection)
+
+function incrementScore(scoreSpan)
 {
-    return { 
-             playerWinsMessage: `You win this round, ${playerSelection} beats ${computerSelection} `,
-             playerLoseMessage: `You loose this round, ${computerSelection} beats ${playerSelection} `,
-             gameTieMessage: 'Its a tie please play again in this round'
-           }   
-};
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
+}
+
+function addSelectionResult(selection, winner)
+{
+    const div = document.createElement('div');
+    div.textContent = selection.name;
+    div.classList.add('result-selection');
+    if(winner) div.classList.add('winner');
+    finalColumn.after(div);
+}
+
+function isWinner(selection, opponentSelection)
+{
+    return selection.beats === opponentSelection.name;
+}
+
+function randomSelection()
+{
+    const randomIndex = Math.floor(Math.random() * gameOptions.length);
+    return gameOptions[randomIndex];
+}
+// const gameOptions = ["rock", "paper", "scissors"],
+//       off = 2,
+//       on = 0;
 
 
-function handlesUserGameRequestInputs()
-{
-    let yes = 1, no = 0;
-    let playerSelection = confirm("Hi this is a rock, paper and Scissors game, do you want to play?");
-    if (playerSelection) return  yes; else return no; 
-}
-function handleUserInputsWhilePlaying()
-{
-    playerSelection = window.prompt("This is a rock, paper, scissors game\nTo play this game make sure you type in only\n Rock,Paper or scissors\nor you wont be allowed to play the current round");
-    console.log(`You have selected ${playerSelection}`);
-    if(playerSelection != null)
-    {
-        playerSelection = playerSelection.toLowerCase().trim();
-        let gameLock = 1;
-        if((playerSelection == gameOptions[0] || playerSelection == gameOptions[1] || playerSelection == gameOptions[2]))
-        {
-            gameLock += 1;
-            return gameLock;
-        }
-        else
-        {
-            gameLock -= 1;
-            return gameLock;
-        }      
-    }
-    else
-    {
-        console.log("We are sorry to say goodbye, but good news you can restart the game by refreshing this page");
-        return -1;
-    }
-}
-let computerPoints = 0,
-playerPoints = 0;
-function evaluatePoints()
-{
-    let Messages = messageResponses(playerSelection, computerSelection);
-    let gameResults;
-    let i;
-    // for(i = 0;;)
-    // {
-    //     gameResults = playRound(playerSelection, computerSelection);
-    //     console.log(gameResults);
-    //     i +=1;        
-    // }
-    // if(gameResults == Messages.playerLoseMessage)
-    // {
-    //     computerPoints += 1;
-    // }
-    // else if(gameResults == Messages.playerWinsMessage)
-    // {
-    //     playerPoints += 1;          
-    // }
-    // else
-    // {
-    //     console.log(`Its a tie no winner of this round`);
-    // }
-    console.log(`playerPoints are: ${playerPoints}`, `computer points are: ${computerPoints}`);
-    return (computerPoints, playerPoints);
+// function computerPlay()
+// {
+//     let computerSelection = gameOptions[Math.floor((Math.random() * gameOptions.length) + 0)];
+//     return computerSelection;
+// }
+
+
+
+// function playRound(playerSelection, computerSelection)
+// {
+//     let Messages = messageResponses(playerSelection, computerSelection);
+//     if( playerSelection == gameOptions[1] && computerSelection == gameOptions[2] )
+//     {
+//         return Messages.playerLoseMessage;
+//     }
+//     else if( playerSelection ==gameOptions[0] && computerSelection == gameOptions[2] )
+//     {
+//         return Messages.playerWinsMessage;
+//     }
+//     else if(playerSelection ==gameOptions[0] && computerSelection == gameOptions[1] )
+//     {
+//         return Messages.playerLoseMessage;
+//     }
+//     else if(playerSelection == gameOptions[2] && computerSelection == gameOptions[1] )
+//     {
+//         return Messages.playerLoseMessage;
+//     }
+//     else if(playerSelection == gameOptions[1] && computerSelection ==gameOptions[0] )
+//     {
+//         return Messages.playerWinsMessage;
+//     } 
+//     else if(playerSelection == gameOptions[2] && computerSelection ==gameOptions[0] )
+//     {
+//         return Messages.playerLoseMessage;
+//     }
+//     else
+//     {
+//         return Messages.gameTieMessage;
+//     }
+    
+// }
+// function messageResponses(playerSelection, computerSelection)
+// {
+//     return { 
+//              playerWinsMessage: `You win this round, ${playerSelection} beats ${computerSelection} `,
+//              playerLoseMessage: `You loose this round, ${computerSelection} beats ${playerSelection} `,
+//              gameTieMessage: 'Its a tie please play again in this round'
+//            }   
+// };
+
+
+// function handlesUserGameRequestInputs()
+// {
+//     let yes = 1, no = 0;
+//     let playerSelection = confirm("Hi this is a rock, paper and Scissors game, do you want to play?");
+//     if (playerSelection) return  yes; else return no; 
+// }
+// function handleUserInputsWhilePlaying()
+// {
+//     playerSelection = window.prompt("This is a rock, paper, scissors game\nTo play this game make sure you type in only\n Rock,Paper or scissors\nor you wont be allowed to play the current round");
+//     console.log(`You have selected ${playerSelection}`);
+//     if(playerSelection != null)
+//     {
+//         playerSelection = playerSelection.toLowerCase().trim();
+//         let gameLock = 1;
+//         if((playerSelection == gameOptions[0] || playerSelection == gameOptions[1] || playerSelection == gameOptions[2]))
+//         {
+//             gameLock += 1;
+//             return gameLock;
+//         }
+//         else
+//         {
+//             gameLock -= 1;
+//             return gameLock;
+//         }      
+//     }
+//     else
+//     {
+//         console.log("We are sorry to say goodbye, but good news you can restart the game by refreshing this page");
+//         return -1;
+//     }
+// }
+// let computerPoints = 0,
+// playerPoints = 0;
+// function evaluatePoints()
+// {
+//     let Messages = messageResponses(playerSelection, computerSelection);
+//     let gameResults;
+//     let i;
+//     // for(i = 0;;)
+//     // {
+//     //     gameResults = playRound(playerSelection, computerSelection);
+//     //     console.log(gameResults);
+//     //     i +=1;        
+//     // }
+//     // if(gameResults == Messages.playerLoseMessage)
+//     // {
+//     //     computerPoints += 1;
+//     // }
+//     // else if(gameResults == Messages.playerWinsMessage)
+//     // {
+//     //     playerPoints += 1;          
+//     // }
+//     // else
+//     // {
+//     //     console.log(`Its a tie no winner of this round`);
+//     // }
+//     console.log(`playerPoints are: ${playerPoints}`, `computer points are: ${computerPoints}`);
+//     return (computerPoints, playerPoints);
     
       
-}
+// }
 
-function checkWinner()
-{
-    computerPoints, playerPoints = evaluatePoints();
-    if(computerPoints > playerPoints)
-    {
-        return `You have Lost, Computer won with ${computerPoints} points`;
-    }
-    else if(computerPoints < playerPoints)
-    {
-        return `You have won with ${playerPoints} points against computers ${computerPoints} points`;
-    }
-    else
-    {
-        return "It's a tie game over";
-    }
-}
-function game()
-{
+// function checkWinner()
+// {
+//     computerPoints, playerPoints = evaluatePoints();
+//     if(computerPoints > playerPoints)
+//     {
+//         return `You have Lost, Computer won with ${computerPoints} points`;
+//     }
+//     else if(computerPoints < playerPoints)
+//     {
+//         return `You have won with ${playerPoints} points against computers ${computerPoints} points`;
+//     }
+//     else
+//     {
+//         return "It's a tie game over";
+//     }
+// }
+// function game()
+// {
     
-    let userChoiceOfWantingToPlay = handlesUserGameRequestInputs();
-    if(userChoiceOfWantingToPlay == 1)
-    {
-        let errorFound = false;
-        do{
-            let inputText = handleUserInputsWhilePlaying();
-            if(inputText == on)
-            {
-                alert("Wrong Input, Please try again");
-            }
-            else if(inputText == -1)
-            {
-                break;
-            }
-            else
-            {
-                //computerSelection = computerPlay();
-                //console.log(`computer has selected ${computerSelection}`);
-                //evaluatePoints();
-                //checkWinner();
-                /*--------------  MAIN LOOP HERE --------------------------------*/
-            }
-        }while(!errorFound)
-    }       
-    else
-    {
-        console.log("We are sorry that you dont want to play, Please reload the page to start the game again");
-    }
-}
+//     let userChoiceOfWantingToPlay = handlesUserGameRequestInputs();
+//     if(userChoiceOfWantingToPlay == 1)
+//     {
+//         let errorFound = false;
+//         do{
+//             let inputText = handleUserInputsWhilePlaying();
+//             if(inputText == on)
+//             {
+//                 alert("Wrong Input, Please try again");
+//             }
+//             else if(inputText == -1)
+//             {
+//                 break;
+//             }
+//             else
+//             {
+//                 //computerSelection = computerPlay();
+//                 //console.log(`computer has selected ${computerSelection}`);
+//                 //evaluatePoints();
+//                 //checkWinner();
+//                 /*--------------  MAIN LOOP HERE --------------------------------*/
+//             }
+//         }while(!errorFound)
+//     }       
+//     else
+//     {
+//         console.log("We are sorry that you dont want to play, Please reload the page to start the game again");
+//     }
+// }
 
-console.log(game());
+// console.log(game());
 
 // function game()
 // {
